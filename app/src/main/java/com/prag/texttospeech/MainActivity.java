d9b13a9a884802fe6440aede89d087eac4f476af
Himanshu507 <HimanshuRoy507@outlook.com>
2019-11-13 20:43:39 +0530

    Something Added:

--- app/src/main/java/com/prag/texttospeech/MainActivity.java
+++ app/src/main/java/com/prag/texttospeech/MainActivity.java
@@ -6,20 +6,25 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Environment;
 import android.speech.tts.TextToSpeech;
+import android.speech.tts.Voice;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private TextToSpeech mTTS;
@@ -31,8 +36,14 @@ public class MainActivity extends AppCompatActivity {
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
 
+    boolean engvoicebool = false, hindivoicebool = true;
+    boolean malevoicebool = false, femalevoicebool = true;
+
     boolean check= false;
     String Filename = "";
+
+    TextView englang, hindilang, femalevoice, malevoice;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,12 +52,18 @@ public class MainActivity extends AppCompatActivity {
         check = checkPermissions();
 
         mButtonSpeak = findViewById(R.id.button_speak);
+        englang = findViewById(R.id.englishlang);
+        hindilang = findViewById(R.id.hindilang);
+        femalevoice = findViewById(R.id.femalevoice);
+        malevoice = findViewById(R.id.malevoice);
+
 
         mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
-                    int result = mTTS.setLanguage(Locale.ENGLISH);
+                    int result = mTTS.setLanguage(new Locale("hin"));
+                    //int result = mTTS.setLanguage(Locale.ENGLISH);
 
                     if (result == TextToSpeech.LANG_MISSING_DATA
                             || result == TextToSpeech.LANG_NOT_SUPPORTED) {
@@ -59,6 +76,123 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        englang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                engvoicebool = true;
+                hindivoicebool = false;
+
+                englang.setTypeface(Typeface.DEFAULT_BOLD);;
+                hindilang.setTypeface(Typeface.DEFAULT);
+
+                mTTS = new TextToSpeech(view.getContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if (status == TextToSpeech.SUCCESS) {
+                            //int result = mTTS.setLanguage(new Locale("hin"));
+                            int result = mTTS.setLanguage(Locale.ENGLISH);
+                            if (result == TextToSpeech.LANG_MISSING_DATA
+                                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("TTS", "Language not supported");
+                            } else {
+                                if (femalevoicebool)
+                                mButtonSpeak.setEnabled(true);
+                                else {
+                                    Set<String> a=new HashSet<>();
+                                    a.add("male");//here you can give male if you want to select male voice.
+                                    Voice v=new Voice("en-us-x-sfg#male_2-local",new Locale("en","US"),400,200,true,a);
+                                    mTTS.setVoice(v);
+                                    mButtonSpeak.setEnabled(true);
+                                }
+                            }
+                        } else {
+                            Log.e("TTS", "Initialization failed");
+                        }
+                    }
+                });
+            }
+        });
+
+
+        hindilang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                engvoicebool = false;
+                hindivoicebool = true;
+
+                hindilang.setTypeface(Typeface.DEFAULT_BOLD);;
+                englang.setTypeface(Typeface.DEFAULT);
+
+                mTTS = new TextToSpeech(view.getContext(), new TextToSpeech.OnInitListener() {
+                    @Override
+                    public void onInit(int status) {
+                        if (status == TextToSpeech.SUCCESS) {
+                            int result = mTTS.setLanguage(new Locale("hin"));
+                            //int result = mTTS.setLanguage(Locale.ENGLISH);
+                            if (result == TextToSpeech.LANG_MISSING_DATA
+                                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                                Log.e("TTS", "Language not supported");
+                            } else {
+                                if (femalevoicebool)
+                                    mButtonSpeak.setEnabled(true);
+                                else {
+                                    Set<String> a=new HashSet<>();
+                                    a.add("male");//here you can give male if you want to select male voice.
+                                    Voice v=new Voice("en-us-x-sfg#male_2-local",new Locale("hin"),400,200,true,a);
+                                    mTTS.setVoice(v);
+                                    mButtonSpeak.setEnabled(true);
+                                }
+                            }
+                        } else {
+                            Log.e("TTS", "Initialization failed");
+                        }
+                    }
+                });
+            }
+        });
+
+
+        malevoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                femalevoicebool = false;
+                malevoicebool = true;
+                malevoice.setTypeface(Typeface.DEFAULT_BOLD);
+                femalevoice.setTypeface(Typeface.DEFAULT);
+
+                    Set<String> a=new HashSet<>();
+                    a.add("male");//here you can give male if you want to select male voice.
+                    Voice v=new Voice("en-us-x-sfg#male_2-local",new Locale("en","US"),400,200,true,a);
+                    mTTS.setVoice(v);
+
+
+            }
+        });
+
+        femalevoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                femalevoice.setTypeface(Typeface.DEFAULT_BOLD);
+                malevoice.setTypeface(Typeface.DEFAULT);
+                malevoicebool = false;
+                femalevoicebool = true;
+                if (hindivoicebool){
+                    Set<String> a=new HashSet<>();
+                    a.add("female");//here you can give male if you want to select mail voice.
+                    Voice v=new Voice("en-us-x-sfg#female_2-local",new Locale("hin"),400,200,true,a);
+                    mTTS.setVoice(v);
+                }else {
+                    Set<String> a=new HashSet<>();
+                    a.add("female");//here you can give male if you want to select mail voice.
+                    Voice v=new Voice("en-us-x-sfg#female_2-local",new Locale("en","US"),400,200,true,a);
+                    mTTS.setVoice(v);
+                }
+            }
+        });
+
 
         mEditText = findViewById(R.id.edit_text);
         mSeekBarPitch = findViewById(R.id.seek_bar_pitch);
@@ -133,6 +267,7 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(),"Your Text is saved in Downlaods folder",Toast.LENGTH_SHORT).show();
     }
+
 
     private boolean checkPermissions() {
         int result;
